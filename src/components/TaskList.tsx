9bb9127c52489a512d014749fa79f609407096cf
Keila Aparecida Rodrigues Passos <keila.rodrigues.passos@gmail.com>
2022-05-23 16:21:40 -0300

    challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,31 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle){
+      let newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks(prevTasks => [...prevTasks, newTask])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    const toggleTaskCompletion = tasks.map((task) => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+    setTasks(toggleTaskCompletion)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const updatedTasks = tasks.filter((task) => task.id !== id)
+    setTasks(updatedTasks)
   }
 
   return (
